6469ee40f13dc98b7616e557aeed2b21aa97e3b1
Arminsharifi4@gmail.com <Arminsharifi4@gmail.com>
2022-08-28 13:35:38 +0430

    Add Legacy WindowsForm Project

---
+++ Src/Windows-Forms-App-Legacy/WhatsAppLinkMaker/Business/CreateLink.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace WhatsAppLinkMaker.Business
+{
+    internal class CreateLink
+    {
+        internal string fncCreateLink(string strPhoneNumber, string strInputText)
+        {
+            if (strPhoneNumber.Length < 11 || strPhoneNumber.Length > 12) return "شماره ورودی اشتباه می باشد";
+            return string.Format("https://wa.me/{0}?text={1}", "98" + strPhoneNumber.Remove(0, 1), strInputText, WebUtility.UrlEncode(strInputText));
+        }
+    }
+}
---
+++ Src/Windows-Forms-App-Legacy/WhatsAppLinkMaker/View/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using WhatsAppLinkMaker.Business;
+using System;
+using System.Windows.Forms;
+
+namespace WhatsAppLinkMaker
+{
+    public partial class MainForm : Form
+    {    
+        // Make Form Moveable by Mouse
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void Panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        // End Make Form Moveable by Mouse
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void picExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void picExit_MouseEnter(object sender, EventArgs e)
+        {
+            picExit.Image = Properties.Resources.icons8_close_window_32px_1;
+        }
+
+        private void picExit_MouseLeave(object sender, EventArgs e)
+        {
+            picExit.Image = Properties.Resources.icons8_close_window_32px;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            fncCreate();
+        }
+
+        private void fncCreate()
+        {
+            txtLink.Text = new CreateLink().fncCreateLink(txtNumber.Text.Replace("-", ""), txtInputText.Text);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            fncClear();
+        }
+
+        private void fncClear()
+        {
+            txtInputText.Text = "";
+            txtLink.Text = "";
+            txtNumber.Text = "";
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            txtNumber.Focus();
+        }
+    }
+}
